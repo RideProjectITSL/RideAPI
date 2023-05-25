5a846f7fcd6d8016e470340322885517d9d1673c
Orlando Martínez <orlandomc19@gmail.com>
2023-05-25 12:23:31 -0600

    puerto y console log

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 app.use(express.json());
-//
 //Rutas
 app.use('/api/v1/ping', (req, res) => {
     res.send('pong');
 });
 //Listen
-const server = app.listen(3000, () => {
-    
+const server = app.listen(() => {
+    console.log('Server online');
 });
